82ef23ec574ed2f786df20d2482acfc33199d4c0
dgl <2433353370@qq.com>
2024-07-03 13:28:31 +0800

    finish registry main

--- main/registry.go
+++ main/registry.go
@@ -9,10 +9,23 @@ import (
 	"net/http"
 )
 
+func displayHelp() {
+	fmt.Println("Usage dprc registry:  -i IPADDRESS -p PORT")
+	fmt.Println("\t-i\tregistry IPv4 or IPv6 address")
+	fmt.Println("\t-p\tregistry port number")
+	fmt.Println("\t-h\thelp message")
+}
 func main() {
+	help := flag.Bool("h", false, "display help")
 	ip := flag.String("i", "127.0.0.1", "ip address")
 	port := flag.Int("p", 8080, "port")
 	flag.Parse()
+
+	if *help {
+		displayHelp()
+		return
+	}
+
 	address := fmt.Sprintf("%s:%d", *ip, *port)
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
